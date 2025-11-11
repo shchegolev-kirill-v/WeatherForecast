b3518b7fdf702885598086f1fa0770b108613d2c
Kirill Shchegolev <shegolev2000@gmail.com>
2025-11-11 13:42:00 +0800

    Removed redundant distinctUntilChanged from bind extension

--- app/src/main/java/org/kshchegolev/weatherforecast/presentation/views/dsl/BindingDsl.kt
+++ app/src/main/java/org/kshchegolev/weatherforecast/presentation/views/dsl/BindingDsl.kt
@@ -3,14 +3,12 @@ package org.kshchegolev.weatherforecast.presentation.views.dsl
 import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.distinctUntilChanged
 
 inline fun <T> View.bind(
     liveData: LiveData<T>,
     crossinline action: View.(T) -> Unit
 ) {
-    val distinctLiveData = liveData.distinctUntilChanged()
-    distinctLiveData.observe(this.context as LifecycleOwner) { value ->
+    liveData.observe(this.context as LifecycleOwner) { value ->
         action(value)
     }
 }
